033da743341b38876408d66bc9ac2e380f15a685
Jason-6602 <92945802+Jason-6602@users.noreply.github.com>
2023-10-18 13:03:13 -0400

    Adding Functionality to retrieve the user reviews and display them on the homepage

--- Reviews/submitreview.php
+++ Reviews/submitreview.php
@@ -1,5 +1,4 @@
 <?php
-
 $host = "localhost";
 $username = "root";
 $password = "mysql";
@@ -13,19 +12,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$rating = $_POST['rating'];
-$comments = $_POST['comments'];
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO reviews (name, email, rating, comments) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssis", $name, $email, $rating, $comments);
 
-// Insert review into the database
-$sql = "INSERT INTO reviews (name, email, rating, comments) VALUES ('$name', '$email', $rating, '$comments')";
+ //Set parameters
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $rating = $_POST['rating'];
+ $comments = $_POST['comments'];
 
-if ($conn->query($sql) === TRUE) {
+
+
+// Output values for debugging
+var_dump($name, $email, $rating, $comments);
+
+// Execute statement and handle errors
+if ($stmt->execute()) {
     echo "Review submitted successfully!";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $stmt->error;
 }
 
 // Close connection
